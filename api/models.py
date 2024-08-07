643597ec27060cee8248569dc20c7bc59cb856a7
Sein Tun <contact@seintun.dev>
2024-08-07 01:11:50 -0700

    Create Post model

--- api/models.py
+++ api/models.py
@@ -8,3 +8,18 @@ class User(AbstractUser):
     """
 
     pass
+
+
+class Post(models.Model):
+    """
+    Post model.
+    """
+
+    title = models.CharField(max_length=255)
+    subheader = models.DateField()
+    text = models.TextField()
+    avatar = models.CharField(max_length=1)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
